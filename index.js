f795b996ca8824efbeb401b399c0dafe706e96a1
MokwaTheDeveloper <moffatmokwa12@gmail.com>
2023-10-01 09:25:56 +0300

    Completed  Assigment

--- index.js
+++ index.js
@@ -1,2 +1,31 @@
 /* This is a file inserted to keep the learn IDE browser happy */
-const testVar = {}
+// Step 1: Uncomment the code to locate the page element (heart)
+const articleHearts = document.querySelectorAll(".like-glyph");
+
+// Step 2: Uncomment the code to set up mock server communication
+function mimicServerCall() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => resolve("success"), 300);
+  });
+}
+
+// Step 3: Uncomment the code to set up event listeners
+function likeCallback(e) {
+  const heart = e.target;
+  mimicServerCall()
+    .then(() => {
+      heart.innerText = FULL_HEART;
+      heart.classList.add("activated-heart");
+    })
+    .catch((error) => {
+      document.getElementById("modal").classList.remove("hidden");
+      document.getElementById("modal-message").innerText = error;
+      setTimeout(() => {
+        document.getElementById("modal").classList.add("hidden");
+      }, 5000);
+    });
+}
+
+for (const heart of articleHearts) {
+  heart.addEventListener("click", likeCallback);
+}
